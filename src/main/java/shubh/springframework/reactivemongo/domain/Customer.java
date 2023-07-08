a284df10a682a081db9a7cc55c40b1b7066f2639
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-08 15:47:47 +0530

    adding entity Customer

---
+++ src/main/java/shubh/springframework/reactivemongo/domain/Customer.java
@@ -0,0 +1,13 @@
+package shubh.springframework.reactivemongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+}
