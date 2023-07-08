58867bc9dd67b6b1c052d6be0d21fb6ee9d86326
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-08 16:15:39 +0530

    adding Data entity for Beer & Customer

--- src/main/java/shubh/springframework/reactivemongo/domain/Beer.java
+++ src/main/java/shubh/springframework/reactivemongo/domain/Beer.java
@@ -6,9 +6,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Beer {
+
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
 }
