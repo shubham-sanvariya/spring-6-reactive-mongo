6e93717c13d97db602c659eec090895fcf7bcdf8
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-09 00:51:13 +0530

    implementing saveBeer method

--- src/main/java/shubh/springframework/reactivemongo/service/BeerServiceImpl.java
+++ src/main/java/shubh/springframework/reactivemongo/service/BeerServiceImpl.java
@@ -1,12 +1,24 @@
 package shubh.springframework.reactivemongo.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import shubh.springframework.reactivemongo.mappers.BeerMapper;
 import shubh.springframework.reactivemongo.model.BeerDTO;
+import shubh.springframework.reactivemongo.repository.BeerRepository;
 
+@Service
+@RequiredArgsConstructor
 public class BeerServiceImpl implements BeerService {
+
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
+
     @Override
-    public Mono<BeerDTO> saveBeer(BeerDTO beerDTO) {
-        return null;
+    public Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDto) {
+        return beerDto.map(beerMapper::beerDtoToBeer)
+                .flatMap(beerRepository::save)
+                .map(beerMapper::beerToBeerDto);
     }
 
     @Override
