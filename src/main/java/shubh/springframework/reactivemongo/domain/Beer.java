313013bda5efd2512e78543653dd1915842250d7
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-08 15:47:33 +0530

    adding entity Beer

---
+++ src/main/java/shubh/springframework/reactivemongo/domain/Beer.java
@@ -0,0 +1,14 @@
+package shubh.springframework.reactivemongo.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+}
