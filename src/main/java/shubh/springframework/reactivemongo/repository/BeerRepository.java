4fadfc9b25b1696786c5dd1fb4bcdf126eaded31
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-10 00:41:09 +0530

    adding findByBeerStyle method in BeerRepo

--- src/main/java/shubh/springframework/reactivemongo/repository/BeerRepository.java
+++ src/main/java/shubh/springframework/reactivemongo/repository/BeerRepository.java
@@ -1,6 +1,7 @@
 package shubh.springframework.reactivemongo.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import shubh.springframework.reactivemongo.domain.Beer;
 
@@ -8,4 +9,5 @@ public interface BeerRepository extends ReactiveMongoRepository<Beer, String > {
 
     Mono<Beer> findFirstByBeerName(String beerName);
 
+    Flux<Beer> findByBeerStyle(String beerStyle);
 }
