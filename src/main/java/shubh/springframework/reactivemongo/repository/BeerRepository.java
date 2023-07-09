6400e6703f9f9b24555f11f68cc79ea89a48758a
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-09 22:34:34 +0530

    adding findFirstByBeerName method in BeerRepo

--- src/main/java/shubh/springframework/reactivemongo/repository/BeerRepository.java
+++ src/main/java/shubh/springframework/reactivemongo/repository/BeerRepository.java
@@ -1,7 +1,11 @@
 package shubh.springframework.reactivemongo.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 import shubh.springframework.reactivemongo.domain.Beer;
 
 public interface BeerRepository extends ReactiveMongoRepository<Beer, String > {
+
+    Mono<Beer> findFirstByBeerName(String beerName);
+
 }
