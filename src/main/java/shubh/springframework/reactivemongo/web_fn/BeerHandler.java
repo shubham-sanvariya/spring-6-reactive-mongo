ca1bd8ff68786ba72d4a9fd1db2d588ee4c12711
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-11 00:46:20 +0530

    adding a qureyParams checking condition in listBeers

--- src/main/java/shubh/springframework/reactivemongo/web_fn/BeerHandler.java
+++ src/main/java/shubh/springframework/reactivemongo/web_fn/BeerHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebInputException;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import shubh.springframework.reactivemongo.model.BeerDTO;
 import shubh.springframework.reactivemongo.service.BeerService;
@@ -77,7 +78,16 @@ public class BeerHandler {
     }
 
     public Mono<ServerResponse> listBeers(ServerRequest request) {
+
+        Flux<BeerDTO> flux;
+
+        if (request.queryParam("beerStyle").isPresent()) {
+            flux = beerService.findByBeerStyle(request.queryParam("beerStyle").get());
+        } else {
+            flux = beerService.listBeers();
+        }
+
         return ServerResponse.ok()
-                .body(beerService.listBeers(), BeerDTO.class);
+                .body(flux, BeerDTO.class);
     }
 }
