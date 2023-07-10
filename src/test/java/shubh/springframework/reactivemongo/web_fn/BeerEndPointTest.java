d17fc17416893f4a666c56e2a2ff564764ab08a5
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-10 15:31:13 +0530

    testing getByBeerId method

--- src/test/java/shubh/springframework/reactivemongo/web_fn/BeerEndPointTest.java
+++ src/test/java/shubh/springframework/reactivemongo/web_fn/BeerEndPointTest.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import shubh.springframework.reactivemongo.model.BeerDTO;
+import shubh.springframework.reactivemongo.service.BeerServiceImplTest;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
@@ -16,6 +22,33 @@ class BeerEndPointTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    void testGetById() {
+        BeerDTO beerDTO = getSavedTestBeer();
+
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("content-type","application/json")
+                .expectBody(BeerDTO.class);
+    }
+
+    public BeerDTO getSavedTestBeer() {
+        FluxExchangeResult<BeerDTO> beerDTOFluxExchangeResult = webTestClient.post()
+                .uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(BeerServiceImplTest.getTestBeer()), BeerDTO.class)
+                .header("Content-Type","application/json")
+                .exchange()
+                .returnResult(BeerDTO.class);
+
+        List<String> location = beerDTOFluxExchangeResult.getRequestHeaders().get("location");
+
+        return webTestClient.get().uri(BeerRouterConfig.BEER_PATH)
+                .exchange()
+                .returnResult(BeerDTO.class)
+                .getResponseBody().blockFirst();
+    }
 
     @Test
     void listBeers() {
