321e7251d52ed7242fc0429b3453296ca5cba7dc
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-10 13:33:05 +0530

    adding BeerHandler with use of Server Response

---
+++ src/main/java/shubh/springframework/reactivemongo/web_fn/BeerHandler.java
@@ -0,0 +1,21 @@
+package shubh.springframework.reactivemongo.web_fn;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import shubh.springframework.reactivemongo.model.BeerDTO;
+import shubh.springframework.reactivemongo.service.BeerService;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
